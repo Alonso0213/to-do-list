63e9327417438073344d740a607f893d83c00ae4
Alonso0213 <alonsocupido51@gmail.com>
2023-06-02 16:33:07 +0200

    not complete

---
+++ script/code.js
@@ -0,0 +1,15 @@
+const display= document.querySelector("#display")
+const capture= document.querySelector("#capture")
+const input= document.querySelector("#input")
+let list =[]
+
+
+// capture.addEventListener("click" (e)=> {
+// let box= document.querySelector("#box")
+// }
+// )
+
+
+// if(input.value!==0){
+//     alert(" please enter items")
+// }
